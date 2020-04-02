54557c55e7c85ff74531dbb01870c8c0f172fa00
Lukasz Wojcik (PGS Software) <lwojcik@pgs-soft.com>
2020-04-02 10:15:03 +0200

    hello world

---
+++ hallo/script.py
@@ -0,0 +1,16 @@
+import math
+
+def lol(a, b):
+    result = a * b
+    return result
+
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+radius = diameter / 2
+
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = pi * math.pow(radius, 2)
+print(lol("1", 2), lol("2", 3) ,lol("3", 4), area)
+# Check your answer
